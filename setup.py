07e846bd49d455271f2eba0acacf643f1c15b6a5
chrisayling <chris.ayling.360@gmail.com>
2025-04-16 10:57:34 +1000

    remove black from prod deps

--- setup.py
+++ setup.py
@@ -188,7 +188,6 @@ setup(
         "dataclasses; python_version<'3.7'",
         "omegaconf==2.4.0.dev2",
         "hydra-core>=1.1",
-        "black",
         "packaging",
         # NOTE: When adding new dependencies, if it is required at import time (in addition
         # to runtime), it probably needs to appear in docs/requirements.txt, or as a mock
